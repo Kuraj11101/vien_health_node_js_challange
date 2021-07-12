b65b6faea68cb5c720d78b8ed8d05cbe562e00e7
Samly Panton <smov.org@gmail.com>
2021-07-12 23:45:51 +0000

    express server config

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const api = require('./app_client/route/api')
 const mongoose = require('mongoose');
 var mongodb = require('mongodb');
-const bodyParser = require('body-parser');
+const bodyparser = require('body-parser');
 const passport = require('passport');
 const jwt = require('./app_client/_helpers/jwt');
 const error_Handler = require('./app_client/_helpers/error_handler');
@@ -94,7 +94,9 @@ app.use(require("express-session")({
 passport.serializeUser(User.serializeUser()); //session encoding
 passport.deserializeUser(User.deserializeUser()); // session decoding
 passport.use(new LocalStrategy(User.authenticate()));
-router.use(bodyParser.json());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
+router.use(bodyparser.json());
 app.use(cors());
 app.use(router);
 
