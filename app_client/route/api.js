0c2e3df0e9aa346e8fea3ba820e3d6e7aeabc7a9
Samly Panton <smov.org@gmail.com>
2021-07-12 23:19:25 +0000

    Initial push

--- app_client/route/api.js
+++ app_client/route/api.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/users/user');
 const mongoose = require('mongoose');
 const db = "mongodb://localhost:27017/data";
+
 mongoose.connect(db, err => {
     if (err) {
         console.log('Error in connect the database' + err);
@@ -16,51 +17,17 @@ router.get('/', (req, res) => {
 });
 
 
-router.post('/register', (req, res) => {
-    User.find({"email": req.body.email})
-        .then(
-            result => {
-                console.log(result.length);
-                if (result.length !== 0) {
-                    res.json({
-                        message: 'Email already exists',
-                        status: false
-                    })
+router.post('/register', (req, res, next ) => {
+    const { email, passwordHash } = req.body;
+    const user = new User({ email });
+    user.setPassword(password);
+    user.save().then(userRecord => {
+        res.json({user: userRecord.toAuthJSON()})
+    }).catch(err => {
+        res.status(500).json({
+            error: err
+        })
+    })
 
-                } else {
-                    let userData = (req.body.name, req.body.email, req.body.password)
-                    let user = new User(userData)
-                    user._id = new mongoose.Types.ObjectId()
-                    console.log(user);
-                    user.save()
-                        .then(
-                            result => {
-                                res.json({
-                                   message: 'User register success',
-                                   status: true,
-                                   send: (userData)
-                                })
-                            }
-                        )
-                        .catch(
-                            error => {
-                                res.json({
-                                   message: ' User Register fail',
-                                   status: false,
-                                })
-                            }
-                        )
-                }
-            }
-        )
-        .catch(
-            error => {
-                res.json({
-                    message: ' User Register fail',
-                    status: false,
-
-                })
-            }
-        )
 });
 module.exports = router;
